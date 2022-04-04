45ac4baeb5719b4fbc5e00947fe2fc3000a12311
Vijay Singh <vijaymanral5@gmail.com>
2022-04-04 18:49:49 +0530

    update models

---
+++ DBEntities/UserRequestModal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDB.Host.DBEntities
+{
+    public class EditUserModal
+    {
+        public User user { get; set; } 
+        public string UserID { get; set; }
+
+    }
+}
---
+++ DBEntities/Users.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoDB.Host.DBEntities
+{
+    public class User
+    {
+        //[BsonId]
+        public BsonObjectId _id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Phone { get; set; }
+        public string UserType { get; set; }
+        public string Role { get; set; }
+        public string DatabaseAccess { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
